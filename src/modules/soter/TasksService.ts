4ad6ad0c77d6b1fe050ad2c0f2a94bd321fa9fc1
Aibek <futuramacoder@gmail.com>
2020-04-16 15:37:26 +0600

    fix: (16.04.20) sync changed

--- src/modules/soter/TasksService.ts
+++ src/modules/soter/TasksService.ts
@@ -31,13 +31,12 @@ export class TasksService {
         this.logger.debug('===================== SYNC =====================');
         const syncTimes = await SyncTime.findAllNotSynced();
         await this.mapService.create();
-        const connection = getConnection();
-        const queryRunner = connection.createQueryRunner();
+        // const connection = getConnection();
+        // const queryRunner = connection.createQueryRunner();
         try {
-            await queryRunner.connect();
-            await queryRunner.startTransaction();
+            // await queryRunner.connect();
+            // await queryRunner.startTransaction();
             for (const syncTime of syncTimes) {
-                console.log(1);
                 const admZip = new AdmZip();
                 const zipPath = `./files/${syncTime.hash}.zip`;
                 const dirPath = this.archiveService.generateDirPath(syncTime.hash);
@@ -51,23 +50,19 @@ export class TasksService {
                     syncTime.entityMapUnSubscribes,
                     syncTime.entityMapComments,
                 );
-                console.log(1.1);
                 if (fs.existsSync(dirPath)) {
                     admZip.addLocalFolder(dirPath, '');
                 }
                 admZip.addFile('map.json', Buffer.from(JSON.stringify(syncTime.fileMap)));
                 admZip.addFile('entities.json', Buffer.from(JSON.stringify(entityMap)));
                 admZip.writeZip(zipPath);
-                console.log(1.3);
                 if (!fs.existsSync(zipPath)) {
-                    console.log(1.4);
                     continue;
                 }
                 const file = fs.readFileSync(zipPath);
                 this.logger.debug('Sync started!');
                 const soterResult = await this.soterService.add(file, syncTime.hash + '.zip');
                 console.log(soterResult);
-                console.log(soterResult.data);
                 if (!soterResult.data.cid || soterResult.data.cid === '') {
                     throw new Error('Cid empty!');
                 }
@@ -88,21 +83,21 @@ export class TasksService {
                         'Content-Type': 'application/json',
                     },
                 }).toPromise();
-                await queryRunner.commitTransaction();
+                // await queryRunner.commitTransaction();
                 this.logger.debug('Soter data: ' + JSON.stringify(soterResult.data));
                 this.logger.debug('Ignite node response status: ' + String(responseIgniteNode.status));
                 this.logger.debug('Sync completed!');
                 // if (syncTime && syncTime.synced === false) {}
             }
         } catch (e) {
-            await queryRunner.rollbackTransaction();
+            // await queryRunner.rollbackTransaction();
             this.logger.error(e.message);
 
             if (e.status === 400) {
                 this.logger.error(e.response.body.data);
             }
         } finally {
-            await queryRunner.release();
+            // await queryRunner.release();
         }
     }
 }
